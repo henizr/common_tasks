c4fbd24a589c289a2501b166782c58207173e39e
Harry <henry.iz@yandex.ru>
2024-08-06 17:42:33 +0300

    Added task 4

--- app.py
+++ app.py
@@ -11,86 +11,61 @@ BACKGROUND_COLOR = "#ffeba1"
 SOUND = ""
 
 
-# координаты x пикселей арбуза
+# координаты x пикселей апельсина
 
-x_coords_when_y_equals_3 = [3, 4, 5, 6, 7, 8]
+x_coords_when_y_equals_1 = [4, 5, 6, 7]
+x_coords_when_y_equals_2 = [3, 4, 5, 6, 7, 8]
+x_coords_when_y_equals_3 = [2, 3, 4, 5, 6, 7, 8, 9]
 x_coords_when_y_equals_4 = [2, 3, 4, 5, 6, 7, 8, 9]
-x_coords_when_y_equals_5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-x_coords_when_y_equals_6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-x_coords_when_y_equals_7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
- 
+x_coords_when_y_equals_5 = [2, 3, 4, 5, 6, 7, 8, 9]
+x_coords_when_y_equals_6 = [2, 3, 4, 5, 6, 7, 8, 9]
+x_coords_when_y_equals_7 = [3, 4, 5, 6, 7, 8]
+x_coords_when_y_equals_8 = [4, 5, 6, 7]
+
+
+
 
 # отрисовка
 def draw(window):
     # Здесь пиши свой код
 
+    # вкладываем, цикл перебирает, выполняет, повторят, переменная цикла # давить на эти слова
+
+    for x in x_coords_when_y_equals_1:
+        Pixel(window, x=x, y=1, color="orange", size=PIXEL_SIZE)
+
+    for x in x_coords_when_y_equals_2:
+        Pixel(window, x=x, y=2, color="orange", size=PIXEL_SIZE)    
+        
     for x in x_coords_when_y_equals_3:
-        Pixel(window, x=x, y=3, color="darkgreen", size=PIXEL_SIZE)
+        Pixel(window, x=x, y=3, color="orange", size=PIXEL_SIZE)
 
     for x in x_coords_when_y_equals_4:
-        if x == 2:
-            Pixel(window, x=x, y=4, color="darkgreen", size=PIXEL_SIZE)
-        elif x == 3:
-            Pixel(window, x=x, y=4, color="green", size=PIXEL_SIZE)
-        elif x == 4:
-            Pixel(window, x=x, y=4, color="green", size=PIXEL_SIZE)
-        elif x == 5:
-            Pixel(window, x=x, y=4, color="green", size=PIXEL_SIZE)
-        elif x == 6:
-            Pixel(window, x=x, y=4, color="green", size=PIXEL_SIZE)
-        elif x == 7:
-            Pixel(window, x=x, y=4, color="green", size=PIXEL_SIZE)
-        elif x == 8:
-            Pixel(window, x=x, y=4, color="green", size=PIXEL_SIZE)
-        elif x == 9:
-            Pixel(window, x=x, y=4, color="darkgreen", size=PIXEL_SIZE)
+        if x == 8:
+            Pixel(window, x=x, y=4, color="yellow", size=PIXEL_SIZE)
+        else:
+            Pixel(window, x=x, y=4, color="orange", size=PIXEL_SIZE)
 
     for x in x_coords_when_y_equals_5:
-        if x == 1:
-            Pixel(window, x=x, y=5, color="darkgreen", size=PIXEL_SIZE)
-        elif x == 2:
-            Pixel(window, x=x, y=5, color="green", size=PIXEL_SIZE)
-        elif x >= 3 and x <= 6:
-            Pixel(window, x=x, y=5, color="red", size=PIXEL_SIZE)
-        elif x == 7:
-            Pixel(window, x=x, y=5, color="black", size=PIXEL_SIZE)
-        elif x == 8:
-            Pixel(window, x=x, y=5, color="red", size=PIXEL_SIZE)
-        elif x == 9:
-            Pixel(window, x=x, y=5, color="green", size=PIXEL_SIZE)
-        elif x == 10:
-            Pixel(window, x=x, y=5, color="darkgreen", size=PIXEL_SIZE)
+        if x == 8:
+            Pixel(window, x=x, y=5, color="yellow", size=PIXEL_SIZE)
+        else:
+            Pixel(window, x=x, y=5, color="orange", size=PIXEL_SIZE)
 
     for x in x_coords_when_y_equals_6:
-        if x == 1:
-            Pixel(window, x=x, y=6, color="darkgreen", size=PIXEL_SIZE)
-        elif x == 2:
+        if x == 6:
             Pixel(window, x=x, y=6, color="green", size=PIXEL_SIZE)
-        elif x == 3:
-            Pixel(window, x=x, y=6, color="red", size=PIXEL_SIZE)
-        elif x == 4:
-            Pixel(window, x=x, y=6, color="black", size=PIXEL_SIZE)
-        elif x >= 5 and x <= 8:
-            Pixel(window, x=x, y=6, color="red", size=PIXEL_SIZE)
-        elif x == 9:
-            Pixel(window, x=x, y=6, color="green", size=PIXEL_SIZE)
-        elif x == 10:
-            Pixel(window, x=x, y=6, color="darkgreen", size=PIXEL_SIZE)
+        else:
+            Pixel(window, x=x, y=6, color="orange", size=PIXEL_SIZE)
 
     for x in x_coords_when_y_equals_7:
-        if x == 1:
-            Pixel(window, x=x, y=7, color="darkgreen", size=PIXEL_SIZE)
-        elif x == 2:
+        if x == 5 or x == 6 or x == 7:
             Pixel(window, x=x, y=7, color="green", size=PIXEL_SIZE)
-        elif x >= 3 and x <= 5:
-            Pixel(window, x=x, y=7, color="red", size=PIXEL_SIZE)
-        elif x == 6:
-            Pixel(window, x=x, y=7, color="black", size=PIXEL_SIZE)
-        elif x ==7:
-            Pixel(window, x=x, y=7, color="red", size=PIXEL_SIZE)
-        elif x == 8:
-            Pixel(window, x=x, y=7, color="red", size=PIXEL_SIZE)
-        elif x == 9:
-            Pixel(window, x=x, y=7, color="green", size=PIXEL_SIZE)
-        elif x == 10:
-            Pixel(window, x=x, y=7, color="darkgreen", size=PIXEL_SIZE)
+        else:
+            Pixel(window, x=x, y=7, color="orange", size=PIXEL_SIZE)
+
+    for x in x_coords_when_y_equals_8:
+        if x == 6:
+            Pixel(window, x=x, y=8, color="green", size=PIXEL_SIZE)
+        else:
+            Pixel(window, x=x, y=8, color="orange", size=PIXEL_SIZE)
